251acba17500da08c97bd2c27c1a8f770e49ce2b
Valegoro502 <valentino.gorosito.olivadoti@gmail.com>
2026-04-15 20:26:13 -0300

    fix de beta + implementacion propia

---
+++ calculator.test.js
@@ -0,0 +1,9 @@
+const { add, subtract } = require('./calculator');
+
+test('Alfa: add(1, 2) should return 3', () => {
+  expect(add(1, 2)).toBe(3);
+});
+
+test('Gamma: subtract(5, 2) should return 3', () => {
+  expect(subtract(5, 2)).toBe(3);
+});
